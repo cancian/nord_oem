4fa683313db8bf2489d04145d2b3ff013b546205
André Cancian <cancian@ateus.net>
2022-01-01 18:15:38 -0300

    Update tweak.sh

--- Ktweak/tweak.sh
+++ Ktweak/tweak.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Written by Draco -- Edited for Nord N10 by Nylar357 (github.com/nylar357)
 
-/bin/sh renice -n -18 -u root system wifi radio
+renice -n -18 -u root system wifi radio
 # terminal feedback
 echo "renicing..."
 
